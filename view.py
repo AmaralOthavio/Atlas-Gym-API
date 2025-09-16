88235c1fa2af9174f8e6a3c91df2112c006fe3e5
AmaralOthavio <167528849+AmaralOthavio@users.noreply.github.com>
2025-09-16 17:28:30 -0300

    Progresso em login, cadastro e banco de dados

--- view.py
+++ view.py
@@ -85,8 +85,10 @@ def informar_verificacao(tipo=0, trazer_pl=False):
 def cadastrar_normal():
     data = request.get_json()
     nome = data.get('nome')
+    senha1 = data.get('senha')
     cpf = data.get('cpf')
     email = data.get('email')
+    email = email.lower()
     tel = data.get('telefone')
     data_nasc = data.get('data_nascimento')
     genero = data.get('genero')
@@ -112,8 +114,55 @@ def cadastrar_normal():
          em 13 dígitos exemplo: +55 (18) 12345-1234 = 5518123451234"""}), 401
     if len(genero) > 100:
         return jsonify({"message": "Limite de dígitos de gênero excedido (100)"}), 401
+    if altura > 2.51 or altura < 0:
+        return jsonify({"message": "Altura inválida"}), 401
+    if '@' not in email:
+        return jsonify({"message": "E-mail inválido"}), 401
+    if peso < 0 or peso > 419:
+        return jsonify({"message": "Peso inválido"}), 401
+    if his_med > 1000:
+        return jsonify({"message": "Limite de caracteres de histórico médico excedido (1000)"}), 401
+    if desc_med > 1000:
+        return jsonify({"message": "Limite de caracteres de descrição de medicamentos excedido (1000)"}), 401
+    if desc_lim > 1000:
+        return jsonify({"message": "Limite de caracteres de descrição de limitações excedido (1000)"}), 401
 
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Verificações de senha
+
+    if len(senha1) < 8:
+        return jsonify({"message": """Sua senha deve conter pelo menos oito caracteres, 
+        uma letra maiúscula e minúscula e um símbolo de seu teclado."""}), 401
+
+    tem_maiuscula = False
+    tem_minuscula = False
+    tem_numero = False
+    tem_caract_especial = False
+    caracteres_especiais = "!@#$%^&*(),-.?\":{}|<>"
+
+    # Verifica cada caractere da senha
+    for char in senha1:
+        if char.isupper():
+            tem_maiuscula = True
+        elif char.islower():
+            tem_minuscula = True
+        elif char.isdigit():
+            tem_numero = True
+        elif char in caracteres_especiais:
+            tem_caract_especial = True
+
+    # Verifica se todos os critérios foram atendidos
+    if not tem_maiuscula:
+        return jsonify({"message": """Sua senha deve conter pelo menos oito caracteres, 
+        uma letra maiúscula e minúscula e um símbolo de seu teclado."""}), 401
+    if not tem_minuscula:
+        return jsonify({"message": """Sua senha deve conter pelo menos oito caracteres, 
+        uma letra maiúscula e minúscula e um símbolo de seu teclado."""}), 401
+    if not tem_numero:
+        return jsonify({"message": """Sua senha deve conter pelo menos oito caracteres, 
+        uma letra maiúscula e minúscula e um símbolo de seu teclado."""}), 401
+    if not tem_caract_especial:
+        return jsonify({"message": """Sua senha deve conter pelo menos oito caracteres, 
+        uma letra maiúscula e minúscula e um símbolo de seu teclado."""}), 401
 
     cur = con.cursor()
     try:
@@ -134,8 +183,56 @@ def cadastrar_normal():
         if resposta[0] == tel:
             return jsonify({"message": "Telefone já cadastrado"}), 401
 
+        senha2 = generate_password_hash(senha1).decode('utf-8')
+
+        cur.execute("""INSERT INTO USUARIOS(NOME, senha, CPF, EMAIL, TELEFONE, DATA_NASCIMENTO, GENERO, ALTURA, 
+        PESO, HISTORICO_MEDICO_RELEVANTE, HISTORICO_MEDICAMENTOS, DESCRICAO_LIMITACOES, TIPO, DESCRICAO_OBJETIVOS,
+        DESCRICAO_TREINAMENTOS_ANTERIORES) (?,?,?,?,?,?,?,?,?,?,?,?,1,?,?)""", (nome, senha2, cpf, email, tel, data_nasc,
+                                                                                genero, altura, peso, his_med, desc_med,
+                                                                                desc_lim, desc_obj, desc_tr))
+        con.commit()
+
     except Exception as e:
         print("Erro em /usuarios/cadastrar")
         return jsonify({"message": "Erro em /usuarios/cadastrar", "erro": f"{e}"}), 500
     finally:
         cur.close()
+
+
+global_contagem_erros = {}
+
+
+@app.route('/login', methods=["POST"])
+def logar():
+    data = request.get_json()
+    email = data.get("email")
+    email = email.lower()
+    senha = data.get("senha")
+
+    cur = con.cursor()
+    # Checando se a senha está correta
+    cur.execute("SELECT senha, id_usuario FROM usuarios WHERE email = ?", (email,))
+    resultado = cur.fetchone()
+
+    if resultado:
+        senha_hash = resultado[0]
+        id_user = resultado[1]
+        ativo = cur.execute("SELECT ATIVO FROM USUARIOS WHERE ID_USUARIO = ?", (id_user,))
+        ativo = ativo.fetchone()[0]
+        if not ativo:
+            return jsonify(
+                {
+                    "message": "Este usuário está inativado.",
+                    "id_user": id_user
+                }
+            ), 401
+
+        if check_password_hash(senha_hash, senha):
+            # Pegar o tipo do usuário para levar à página certa
+            tipo = cur.execute("SELECT TIPO FROM USUARIOS WHERE ID_USUARIO = ?", (id_user,))
+            tipo = tipo.fetchone()[0]
+            token = generate_token(id_user)
+            # Excluir as tentativas que deram errado
+            id_user_str = f"usuario-{id_user}"
+            if id_user_str in global_contagem_erros:
+                del global_contagem_erros[id_user_str]
